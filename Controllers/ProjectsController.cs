c79d18aa5a5604244bf4fd9d693fd851cee4bc2f
mpatel4ldata <mpatel@4ldata.com>
2025-12-24 13:55:26 +0530

    Menu changes in all pages

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -126,6 +126,16 @@ namespace DataSync.Web.Controllers
             // IMPORTANT: render Verify.cshtml, not Details.cshtml
             return View(vm); // will load Views/Projects/Verify.cshtml
         }
+        public IActionResult Entityconfig(int id)
+        {
+            var vm = BuildProjectDetailsVm(id);
+            if (vm == null) return NotFound();
+
+            ViewData["ActiveTab"] = "Config";
+
+            // IMPORTANT: render Verify.cshtml, not Details.cshtml
+            return View(vm); // will load Views/Projects/Verify.cshtml
+        }
         // --------------------------------------------
         // TEMP: mock builder (later replace with DB/ADF)
         // --------------------------------------------
